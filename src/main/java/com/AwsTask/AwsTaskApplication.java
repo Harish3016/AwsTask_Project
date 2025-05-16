4b08c91fd70989e1c88e547c11a58e0d14008ef9
Harish3016 <harishr3016@gmail.com>
2025-05-16 15:00:18 -0700

    initial-commit

---
+++ src/main/java/com/AwsTask/AwsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.AwsTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/AwsTask/Controller/AwsController.java
@@ -0,0 +1,15 @@
+package com.AwsTask.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AwsController {
+	
+	@GetMapping(value="/getName/{n}")
+	public String getName(@PathVariable String name) {
+		return name;
+	}
+
+}
